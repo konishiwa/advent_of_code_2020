69269c2931a809b5407849aef4b302c8653335b2
Konishi, Ruka <RKonishi@cvent.com>
2020-12-09 13:26:26 -0800

    fix: add days

--- days/day7/puzzle1/main.go
+++ days/day7/puzzle1/main.go
@@ -3,14 +3,8 @@ package main
 import (
 	"bufio"
 	"os"
-	"strconv"
 	"strings"
 )
-
-type command struct {
-	function string
-	steps    int
-}
 
 //checks if file exists
 func check(e error) {
@@ -19,7 +13,11 @@ func check(e error) {
 	}
 }
 
-func readInFile() [594]command {
+type contents struct {
+	bags []string
+}
+
+func readInFile() {
 
 	//read the values in
 	file, err := os.Open("input.txt")
@@ -34,56 +32,59 @@ func readInFile() [594]command {
 
 	file.Close()
 
-	//store in multidimensional array
-	var commands [594]command
-
 	//map that holds how many times a line has been called
-	bags := make(map[string]string)
+	//bags := make(map[string]contents)
 
-	//example lines
-	// 	shiny indigo bags contain 4 vibrant lime bags.
-	// clear lime bags contain 1 dotted lime bag, 2 clear gold bags.
-	// dotted turquoise bags contain 2 shiny green bags, 5 striped magenta bags, 3 muted green bags.
+	//set all bag contents
+	for _, each_ln := range text {
 
-	for i, each_ln := range text {
-
-		//clean the data
 		//if its: contain no other bags - we're not interested
 		if !strings.Contains(each_ln, "contain no other bags.") {
 
+			//clean the string up - remove whats not needed
+			cleanLn := strings.NewReplacer(each_ln, "bags", " ",
+				each_ln, "bag", " ",
+				each_ln, "contains", " ")
+
+			println(cleanLn)
+
 			//split string
-			s := strings.Split(each_ln, " ")
+			//s := strings.Split(each_ln, " ")
 			//first two words are the type of bag
-			key := s[0] + s[1]
-			//add to bags 
-			
-			//depending on the value x add x times
-			for a = append(a, iter.Val)
+			//key := s[0] + s[1]
 
+			// 	var contents string[]
+
+			// 	//TODO: find a way to prevent string manipulation
+			// 	//ignore  next two words: bags contains
+			// 	cont := true
+			// 	bagIndex := 4
+			// 	for cont {
+
+			// 		numBags, _ := strconv.Atoi(s[bagIndex])
+			// 		bagLen := len(s[bagIndex+2])
+
+			// 		if strings.Contains(s[bagIndex+3], ".") {
+			// 			cont = false
+			// 		}
+
+			// 		for x := 0; x < numBags; x++ {
+
+			// 			content_key := s[bagIndex+1] + s[bagIndex+2]
+			// 			contents = append(contents, content_key)
+			// 		}
+			// 		bagIndex += 4
+			// 		bags[key] = contents
+			// 	}
+			// }
 		}
 
-		//first two lines are what it can be kept in
-
-		//the function
-		function := s[0]
-
-		if function != "nop" {
-			//the steps
-			steps, _ := strconv.Atoi(s[1])
-			//set command struct
-			commands[i] = command{function, steps}
-		} else {
-			commands[i] = command{function, 0}
-		}
 	}
-	return commands
 }
 
 func main() {
 
 	//get commands
-	commands := readInFile()
-	//map that holds how many times a line has been called
-	lineCall := make(map[int]int)
+	readInFile()
 
 }
