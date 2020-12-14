58c9abc7ddc5b6bf19e0ebb46504e63691edd098
Haruka Konishi <harukakonishi@citadelmac.local>
2020-12-13 23:24:39 -0800

    fix: day 12 got example to work

--- days/day12/puzzle1/main.go
+++ days/day12/puzzle1/main.go
@@ -34,9 +34,9 @@ func parseDirections(input []string) []Direction {
 	return directions
 }
 
-func parseInputFromFile() []Direction {
+func parseInputFromFile(path string) []Direction {
 
-	file, _ := os.Open("input.txt")
+	file, _ := os.Open(path)
 	defer file.Close()
 
 	var lines []string
@@ -50,50 +50,48 @@ func parseInputFromFile() []Direction {
 func printManhattenDirections(directions []Direction) {
 	//store which way the ship is faced
 	face := "E"
-	//assuming L and R are always 90
-	var faceMap = map[string]string{
-		"ER90":  "S",
-		"EL90":  "N",
-		"SL90":  "E",
-		"SR90":  "W",
-		"NL90":  "W",
-		"NR90":  "E",
-		"WL90":  "S",
-		"WR90":  "N",
-		"ER180": "W",
-		"EL180": "W",
-		"SL180": "N",
-		"SR180": "N",
-		"NL180": "S",
-		"NR180": "S",
-		"WL180": "E",
-		"WR180": "E",
+
+	var compassL = map[string][]string{
+		"E": {"N", "W", "S"},
+		"N": {"W", "S", "E"},
+		"W": {"S", "E", "N"},
+		"S": {"E", "N", "W"},
+	}
+
+	var compassR = map[string][]string{
+		"E": {"S", "W", "N"},
+		"N": {"E", "S", "W"},
+		"W": {"N", "E", "S"},
+		"S": {"W", "N", "E"},
 	}
 
 	//total distance
 	var longitude int
 	var lattitude int
+	direction := face
 
 	for _, s := range directions {
 
-		direction := face
+		println("current direction: ", direction)
 
-		if s.direction == "L" || s.direction == "R" {
-			//println("old direction: ", direction)
-			steps := strconv.FormatInt(int64(s.steps), 10)
-			key := face + s.direction + steps
-			//println("key: ", key)
-			//println("s.steps: ", s.steps)
-			direction = faceMap[key]
-			//println("new direction: ", direction)
+		if s.direction == "L" {
+
+			steps := s.steps/90 - 1
+			possibleDirections := compassL[direction]
+			face = possibleDirections[steps]
+
+		} else if s.direction == "R" {
+
+			steps := s.steps/90 - 1
+			possibleDirections := compassR[direction]
+			face = possibleDirections[steps]
+
 		} else {
 			if s.direction != "F" {
 				direction = s.direction
+			} else {
+				direction = face
 			}
-			println("direction: ", direction)
-			println("steps: ", s.steps)
-			println("old longitude: ", longitude)
-			println("old lattitude: ", lattitude)
 			switch direction {
 			case "N":
 				longitude += s.steps
@@ -104,8 +102,6 @@ func printManhattenDirections(directions []Direction) {
 			case "E":
 				lattitude -= s.steps
 			}
-			println("new longitude: ", longitude)
-			println("new lattitude: ", lattitude)
 		}
 	}
 	total := math.Abs(float64(longitude)) + math.Abs(float64(lattitude))
@@ -114,7 +110,7 @@ func printManhattenDirections(directions []Direction) {
 
 func main() {
 
-	directions := parseInputFromFile()
+	directions := parseInputFromFile("example.txt")
 	printManhattenDirections(directions)
 
 }
